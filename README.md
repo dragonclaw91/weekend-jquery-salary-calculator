4b8864d22ad1c0eeddc63be039a6c800a093a101
David Brown <davidbrown@davids-MBP.hitronhub.home>
2022-05-08 09:54:26 -0500

    completed weekend-jquery-salary-calculator

--- README.md
+++ README.md
@@ -1,9 +1,26 @@
 # Project Name
-
-[Project Instructions](./INSTRUCTIONS.md), this line may be removed once you have updated the README.md
+weekend-jquery-salary-calculator
 
 ## Description
 
 Your project description goes here. What problem did you solve? How did you solve it?
 
-Additional README details can be found [here](https://github.com/PrimeAcademy/readme-template/blob/master/README.md).
+the biggest problem ifaced when makeing this was getting the right things that were previoulsy deleted or already on the tale from being added when the submit button was clicked
+
+I sloved this problem by adding before the for loop was run 
+and after the delte function had run
+
+ ## Installation
+
+pretty straight forward just get the files downloaded and open the index.html in browser
+
+ ## Usage
+
+this is pretty stright forward in terms of funcionality it takes user input from the fields and adds them to a table it then adds the total monthly salary form all the inputed data if the monthly total exceeds 20k it lets the alerts the user to over spending by turning the amount displayed red
+
+
+ ## Acknowledgement
+Thanks to Emerging Digital Academy who equipped and helped me to make this application a reality. (Thank your people)
+
+ ## Support
+If you have suggestions or issues, please email me at atr1234@hotmail.com
---
+++ script.js
@@ -0,0 +1,83 @@
+console.log("good to go");
+
+// create a redy on function for jquery
+
+$(document).ready(readyOn)
+
+function readyOn() {
+    /* first set it up so when the user inputs anything into the fields
+     it gets put in an object and pushed into an array to be looped over later */
+
+
+    $("#submittButton").on("click", function () {
+        employee.push({
+            fName: $("#firstName").val(),
+            lName: $("#lastName").val(),
+            ID: $("#id").val(),
+            title: $("#title").val(),
+            annualSalary: $("#annualSalary").val(),
+        });
+        // this will clear the input fields after the submit button is clicked
+        $("#firstName").val("")
+        $("#lastName").val("")
+        $("#id").val("")
+        $("#title").val("")
+        $("#annualSalary").val("")
+        addEmployee(employee);
+    });
+    /* attaching a listner to to the table body so when the delete button is clicked it 
+    deletes just the row it was clicked on*/
+
+    $("#tableBody").on("click", "#delete", deleteTable);
+}
+// defining global variables to be updated and appended at the end of the code 
+
+let totalMonthly = 0;
+let sum = 0;
+let employee = [];
+
+
+/* empties the tabl so it only adds what is added
+ to the employees array in between submitt button clicks */
+
+// then it loops over each item in the employee array and appends them to the table body 
+
+//then adds to the global sum so it can keep track of the total monthly spenditure
+function addEmployee(employees) {
+    $("#tableBody").empty();
+    for (let i = 0; i < employees.length; i++) {
+        $("#tableBody").append(
+            `<tr><td>` + employees[i].fName + `</td><td>` + employees[i].lName
+            + `</td><td>` + employees[i].ID + `</td><td>` + employees[i].title
+            + `</td><td>` + employees[i].annualSalary + `</td><td>`
+            + `<button id = "delete"> delete </button></td></tr>`);
+        sum = Number(employees[i].annualSalary)
+    };
+
+    // takes the global sum and updates the total monthly variable and appends it the th DOM
+    totalMonthly += sum;
+    $("#totalSum").text(totalMonthly);
+    /* sets it up so the if ttotal monthly gets over 20k
+     it colors the text red to alert the user to over spending */
+
+    if (totalMonthly > 20000) {
+        $("#totalSum").css("color", "red")
+    };
+}
+//sets up the delete function to delete the row that it was cliocked on 
+
+/*needs to have the event passed in as an argument becuse
+ the delete button dosent exist when the page loads 
+
+ finally sets the employee array an empty array to ensure nothing uninentional gets added*/
+
+function deleteTable(event) {
+    $(event.target).closest('tr').remove();
+    $("#totalSum").text(totalMonthly);
+    employee = []
+};
+
+
+
+
+
